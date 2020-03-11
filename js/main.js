5d1491f539f385e3e4a0e9f94fa470dc5d339e68
AlexHlukhyi <AlexHlukhyi@users.noreply.github.com>
2020-03-11 15:15:56 +0200

    v0.1 stable, working with localStorage

--- js/main.js
+++ js/main.js
@@ -1,38 +1,88 @@
+var data = [];
+
 window.onload = function() {
-    console.dir(loadData());
-    // localStorage['06.03.2020'] = JSON.stringify([
-    //     {
-    //         'name': 'Note1',
-    //         'description': 'desc1'
-    //     },
-    //     {
-    //         'name': 'Note2',
-    //         'description': 'desc2'
-    //     }
-    // ]);
-    // var list = document.getElementById('notes');
-    // for (var i = 0; i < localStorage.length; i++) {
-    //     var dateField = document.createElement('div');
-    //     var date = localStorage.key(i);
-    //     dateField.id = date;
-    //     dateField.innerHTML = date;
-    //     var notesField = document.createElement('ul');
-    //     var notes = JSON.parse(localStorage.getItem(date));
-    //     for (var note in notes) {
-    //         var noteField = document.createElement('li');
-    //         noteField.innerHTML = notes[note].name;
-    //         notesField.appendChild(noteField);
-    //     }
-    //     dateField.appendChild(notesField);
-    //     list.appendChild(dateField);
-    // }
+    data = loadData();
+    showData(data);
 };
 
 function loadData() {
-    var data = [];
-    for (var i = 0; i < localStorage.length; i++) {
-        var date = localStorage.key(i);
+    let data = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        let date = localStorage.key(i);
         data[date] = JSON.parse(localStorage.getItem(date));
     }
     return data;
+}
+
+function showData(data) {
+    let list = document.getElementById('notes');
+    for (let date in data) {
+        let dateField = document.createElement('div');
+        dateField.id = date;
+        dateField.innerHTML = date;
+        let notesField = document.createElement('ul');
+        let notes = data[date];
+        for (let note in notes) {
+            let noteField = document.createElement('li');
+            // creating span with name of note
+            let noteName = document.createElement('span');
+            noteName.innerHTML = notes[note].name;
+            noteName.className = "note-name";
+            noteField.appendChild(noteName);
+            // creating show button
+            let showButton = document.createElement('button');
+            showButton.innerHTML = 'show';
+            showButton.className = "btn btn-primary btn-sm";
+            showButton.onclick = function () {
+                showNote(note, date, notes[note].name, notes[note].description);
+            };
+            noteField.appendChild(showButton);
+            // creating delete button
+            let deleteButton = document.createElement('button');
+            deleteButton.innerHTML = 'delete';
+            deleteButton.className = "btn btn-danger btn-sm";
+            deleteButton.onclick = function () {
+                deleteNote(note, date);
+            };
+            noteField.appendChild(deleteButton);
+            //
+            notesField.appendChild(noteField);
+        }
+        dateField.appendChild(notesField);
+        list.appendChild(dateField);
+    }
+}
+
+function showNote(id, date, name, description) {
+    document.getElementById('note').style.display = 'block';
+    document.getElementById('note-date').innerHTML = date;
+    document.getElementById('note-name').innerHTML = name;
+    document.getElementById('note-desc').innerHTML = description;
+    document.getElementById('edit-button').onclick = function () {
+        editNote(id, date);
+    }
+}
+
+function addNote() {
+    let date = document.getElementById('new-note-date').value;
+    let name = document.getElementById('new-note-name').value;
+    let note = {'name' : name, 'description' : ''};
+    if (!data[date]) {
+        data[date] = [];
+    }
+    data[date].push(note);
+    localStorage.setItem(date, JSON.stringify(data[date]));
+    document.location.reload();
+}
+
+function editNote(id, date) {
+    data[date][id].description = document.getElementById('note-desc').value;
+    localStorage.setItem(date, JSON.stringify(data[date]));
+    document.location.reload();
+}
+
+function deleteNote(id, date) {
+    data[date].splice(id);
+    localStorage.setItem(date, JSON.stringify(data[date]));
+    document.location.reload();
 }
