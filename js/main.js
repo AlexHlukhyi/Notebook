5bd46f3ed643649a332810e08251f2a641bf8fce
AlexHlukhyi <AlexHlukhyi@users.noreply.github.com>
2020-03-12 13:16:14 +0200

    v0.2 using this inside the object context fixed
    - using this inside callbacks checked

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let NotesSystem = {
     data: [],
-    init: function() {
-        NotesSystem.data = NotesSystem.loadData();
-        NotesSystem.showData();
+    init: function () {
+        this.data = this.loadData();
+        this.showData();
     },
-    loadData: function() {
+    loadData: function () {
         let data = [];
         for (let i = 0; i < localStorage.length; i++) {
             let date = localStorage.key(i);
@@ -12,8 +12,11 @@ let NotesSystem = {
         }
         return data;
     },
-    showData: function() {
-        document.getElementById('note').style.display = 'hidden';
+    showData: function () {
+        document.getElementById('note-date').innerHTML = '';
+        document.getElementById('note-name').innerHTML = '';
+        document.getElementById('note-desc').value = '';
+        document.getElementById('note').style.display = 'none';
         let list = document.getElementById('notes');
         list.innerHTML = "";
         for (let date in this.data) {
@@ -21,7 +24,7 @@ let NotesSystem = {
             dateField.id = date;
             dateField.innerHTML = date;
             let notesField = document.createElement('ul');
-            let notes = NotesSystem.data[date];
+            let notes = this.data[date];
             for (let note in notes) {
                 let noteField = document.createElement('li');
                 // creating span with name of note
@@ -52,40 +55,42 @@ let NotesSystem = {
             list.appendChild(dateField);
         }
     },
-    showNote: function(id, date, name, description) {
+    showNote: function (id, date, name, description) {
         document.getElementById('note').style.display = 'block';
         document.getElementById('note-date').innerHTML = date;
         document.getElementById('note-name').innerHTML = name;
-        document.getElementById('note-desc').innerHTML = description;
+        document.getElementById('note-desc').value = description;
         document.getElementById('edit-button').onclick = function () {
             NotesSystem.editNote(id, date);
         }
     },
-    addNote: function() {
+    addNote: function () {
         let date = document.getElementById('new-note-date').value;
         let name = document.getElementById('new-note-name').value;
         let note = {'name' : name, 'description' : ''};
-        if (!NotesSystem.data[date]) {
-            NotesSystem.data[date] = [];
+        if (!this.data[date]) {
+            this.data[date] = [];
         }
-        NotesSystem.data[date].push(note);
-        localStorage.setItem(date, JSON.stringify(NotesSystem.data[date]));
-        NotesSystem.init();
+        this.data[date].push(note);
+        localStorage.setItem(date, JSON.stringify(this.data[date]));
+        this.init();
     },
     editNote: function(id, date) {
-        NotesSystem.data[date][id].description = document.getElementById('note-desc').value;
-        localStorage.setItem(date, JSON.stringify(NotesSystem.data[date]));
-        NotesSystem.init();
+        this.data[date][id].description = document.getElementById('note-desc').value;
+        localStorage.setItem(date, JSON.stringify(this.data[date]));
+        this.init();
     },
     deleteNote: function(id, date) {
-        NotesSystem.data[date].splice(id, 1);
-        if (NotesSystem.data[date].length < 1) {
+        this.data[date].splice(id, 1);
+        if (this.data[date].length < 1) {
             localStorage.removeItem(date);
         } else {
-            localStorage.setItem(date, JSON.stringify(NotesSystem.data[date]));
+            localStorage.setItem(date, JSON.stringify(this.data[date]));
         }
-        NotesSystem.init();
+        this.init();
     }
 };
 
-window.onload = NotesSystem.init;
+window.onload = function () {
+    NotesSystem.init();
+};
