34a7c84c0772c8be92fc1ccd74a4ee3d360bbd0e
AlexHlukhyi <AlexHlukhyi@users.noreply.github.com>
2020-05-26 10:20:34 +0300

    v0.5 modal windows appear
    changed fonts, fixed issues

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,24 @@
-function showSideBar() {
-	document.getElementById('side-bar').style.display = 'flex';
-	document.getElementById('overlay').style.display = 'block';
-}
-function closeSideBar() {
-	document.getElementById('side-bar').removeAttribute('style');
-	document.getElementById('overlay').removeAttribute('style');
-}
+document.addEventListener('DOMContentLoaded',function () {
+	var modalButtons = document.querySelectorAll('.modal-button');
+	var overlay = document.querySelector('.overlay');
+	var currentModal;
+
+	modalButtons.forEach(function(button){
+		button.addEventListener('click', function(e) {
+			e.preventDefault();
+			let data = this.getAttribute('data-modal');
+			if (data === 'side-bar') {
+				currentModal = document.querySelector('.side-bar');
+			} else {
+				currentModal = document.querySelector('.modal[data-modal="' + data + '"]');
+			}
+			currentModal.classList.add('active');
+			overlay.classList.add('active');
+		});
+	});
+
+	overlay.addEventListener('click', function(e) {
+		currentModal.classList.remove('active');
+		overlay.classList.remove('active');
+	});
+});
